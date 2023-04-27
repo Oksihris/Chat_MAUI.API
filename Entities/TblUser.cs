07126f2e044fadb213f90570ebd5ca00c9583d2e
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-04-27 19:40:57 +0300

    Добавьте файлы проекта.

---
+++ Entities/TblUser.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatAppAPI.Entities
+{
+    public class TblUser
+    {
+        public int Id { get; set; } 
+        public string LoginId { get; set; } 
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public byte[] StoreSalt { get; set; }       
+
+        public string AvatarSourceName { get; set; }
+    }
+}

bbd5c2ed9d451b1ea5422c96fdd6a497e47c042b
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-05-08 17:57:42 +0300

    userDB

--- Functions/User/User.cs
+++ Functions/User/User.cs
@@ -2,5 +2,19 @@
 {
     public class User
     {
+
+        public int Id { get; set; }
+      
+        public string UserName { get; set; } = null;
+
+        public string Password { get; set; } = null;
+
+
+        public string AvatarSourceName { get; set; } = null;
+
+        public bool IsOnline { get; set; }
+
+        public DateTime LastLogonTime { get; set; }
+
     }
 }
